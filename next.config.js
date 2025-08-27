4dbf5ca085f18b3f30ecc45030fbb4fd8368cd8b
Anuskawebb <anuskasarkarofficial@gmail.com>
2025-08-27 17:07:58 +0530

    Fixed Next Build error

--- next.config.js
+++ next.config.js
@@ -18,6 +18,14 @@ const nextConfig = {
     };
     return config;
   },
+  // Disable tracing to avoid permission issues
+  generateBuildId: async () => {
+    return 'build-' + Date.now();
+  },
+  // Moved from experimental section as per Next.js recommendation
+  outputFileTracingExcludes: {
+    '*': ['node_modules/**/*'],
+  },
 }
 
-module.exports = nextConfig 
+module.exports = nextConfig
